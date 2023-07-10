aec9c8d755e28c38f93184f053fe54c51eb7fc36
HuseynFarzali <huseynyf@code.edu.az>
2023-07-10 02:58:05 -0700

    Add project files.

---
+++ AcademyProject_String_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AcademyProject_String_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+            Console.WriteLine("Detected words are:");
+            foreach (string word in words)
+            {
+                Console.Write(" " + word);
+            }
+            Console.WriteLine('\n');
+
+            Console.WriteLine("Total number of words in the sentence: " + words.Length);
+        }
+    }
+}
